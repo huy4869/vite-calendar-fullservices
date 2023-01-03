8fee4314eabc19156a15cf4f32f1eeda25453dc8
Trần Việt Huy <79585798+tranviethuy04@users.noreply.github.com>
2023-01-03 23:05:59 +0700

    up aos

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const app = createApp(App);
 
 app.use(router);
+// @ts-ignore
+app.use(AOS.init())
 
 app.mount("#app");
