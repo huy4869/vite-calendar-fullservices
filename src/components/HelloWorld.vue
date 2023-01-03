d95f5cce4578a3d8c444874ce4ec2029a8b36649
Trần Việt Huy <79585798+tranviethuy04@users.noreply.github.com>
2023-01-04 01:02:31 +0700

    up tailwindcss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,13 +6,30 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p class="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p class="text-gray-500 text-lg">
+        Vue and Tailwind CSS in action
+      </p>
+    </div>
+    <h1 class="green text-3xl">{{ msg }} helo</h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
       What's next?
     </h3>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div class="flex-shrink-0">
+        <img class="h-12 w-12" src="/favicon.ico" alt="ChitChat Logo">
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import './assets/index.scss'
+
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
