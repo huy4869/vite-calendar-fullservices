d1d00e76e16b682b99a47d8e3898ecf0b4309fd2
huy4869 <huytv@amaisoft.com>
2023-01-05 13:46:11 +0700

    add husky

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
 
 // scss
-import "./assets/main.css";
+import './assets/main.css'
 import './assets/index.scss'
 
 // aos animation
@@ -11,20 +13,18 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 // particles-js
-import Particles from "vue3-particles"
+import Particles from 'vue3-particles'
 
 // element plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(router);
+app.use(createPinia())
+app.use(router)
 // @ts-ignore
 app.use(AOS.init())
-
 app.use(Particles)
-
 app.use(ElementPlus)
-
-app.mount("#app");
+app.mount('#app')
